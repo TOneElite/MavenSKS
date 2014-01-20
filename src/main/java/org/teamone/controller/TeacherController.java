dc9a2a2a64adcdb951b5be5716460d7ec80e9106
ikhagen <idakhagen@gmail.com>
2014-01-20 17:43:43 +0100

    Lærer

--- src/main/java/org/teamone/controller/TeacherController.java
+++ src/main/java/org/teamone/controller/TeacherController.java
@@ -1,5 +1,6 @@
 package org.teamone.controller;
 
+import java.util.Date;
 import java.util.List;
 import java.util.StringTokenizer;
 import javax.swing.JOptionPane;
@@ -140,6 +141,7 @@ public class TeacherController {
                     userTask.setEmail(temp[0]);
                     userTask.setSubjectCode(subjectCode);
                     userTask.setTaskNr(Integer.parseInt(temp[1]));
+                    userTask.setDate(new Date());
                     userTasksJDBCTemplate.approve(userTask);
                     System.out.println("dette er temp 1 " + temp[1] + " dette er temp 0 " + temp[0]);
                 }
@@ -147,8 +149,8 @@ public class TeacherController {
                 UserTask userTask = new UserTask();
                 userTask.setEmail(tasks[0]);
                 userTask.setSubjectCode(subjectCode);
-
                 userTask.setTaskNr(Integer.parseInt(tasks[1]));
+                userTask.setDate(new Date());
                 userTasksJDBCTemplate.approve(userTask);
                 System.out.println("dette er tasks 1 " + tasks[1] + " dette er tasks 0 " + tasks[0]);
             }
