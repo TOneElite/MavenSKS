7eaff9e5a6036e660b956b13191659c55228f192
Dagfinn Reitan <dagfinsr@student.hist.no>
2014-01-21 15:48:46 +0100

    Room image switching when you switch rooms. Menu subjects doesn't disappear when you click them.

--- src/main/java/org/teamone/controller/HomeController.java
+++ src/main/java/org/teamone/controller/HomeController.java
@@ -86,6 +86,7 @@ public class HomeController {
         // Check for admin rights
         boolean admin = false;
         boolean teacher = false;
+        boolean user = false;
         for (GrantedAuthority ga : auth.getAuthorities()) {
             if (ga.toString().equals("ROLE_ADMIN")) {
                 System.out.println("is ADMIN!");
@@ -96,6 +97,21 @@ public class HomeController {
                 System.out.println("is TEACHER!");
                 teacher = true;
                 model.addAttribute("isTeacher", teacher);
+            }
+            if (ga.toString().equals("ROLE_USER")) {
+                System.out.println("is USER!");
+                user = true;
+                model.addAttribute("isUser", user);
+            }
+            if (ga.toString().equals("ROLE_USER")) {
+                System.out.println("is USER!");
+                user = true;
+                model.addAttribute("isUser", user);
+            }
+            if (ga.toString().equals("ROLE_USER")) {
+                System.out.println("is USER!");
+                user = true;
+                model.addAttribute("isUser", user);
             }
         }
 
