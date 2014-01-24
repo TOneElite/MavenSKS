53b18ce6c6ee0cacfa3ca8c68931f834f2355143
Kim Andre Kjelsberg <kakjels@gmail.com>
2014-01-24 09:28:35 +0100

    Prepared and configured necessary test classes

---
+++ src/test/java/org/teamone/logic/RuleServiceTest.java
@@ -0,0 +1,64 @@
+
+package org.teamone.logic;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author Kim
+ */
+@Ignore("Test is a prototype, skip for now")
+public class RuleServiceTest {
+	
+	
+	@Before
+	public void setUp() {
+	}
+	
+	@After
+	public void tearDown() {
+	}
+
+	/**
+	 * Test of replaceLast method, of class RuleService.
+	 */
+	@Test
+	public void testReplaceLast() {
+		
+	}
+
+	/**
+	 * Test of readTabletoRules method, of class RuleService.
+	 */
+	@Test
+	public void testReadTabletoRules() {
+		
+	}
+
+	/**
+	 * Test of readRuleString method, of class RuleService.
+	 */
+	@Test
+	public void testReadRuleString() {
+		
+	}
+
+	/**
+	 * Test of vertifyRequirements method, of class RuleService.
+	 */
+	@Test
+	public void testVertifyRequirements() {
+		
+	}
+
+	/**
+	 * Test of readRulesNOR method, of class RuleService.
+	 */
+	@Test
+	public void testReadRulesNOR() {
+		
+	}
+}
